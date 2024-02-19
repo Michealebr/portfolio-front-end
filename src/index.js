fa0c7b6a1ce02cca59e9f1ee569fe6935a9033d6
Michealebr <michael.ebrahimi77@gmail.com>
2024-02-19 19:02:02 +0000

    commit

--- src/index.js
+++ src/index.js
@@ -1,61 +1,92 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import PersonalSite from './pages/PersonalSite';
+// import Aestige from './pages/Aestige.jsx';
+// import Stacked from './pages/Stacked.jsx';
+// import {
+//   BrowserRouter as Router,
+//   createBrowserRouter,
+//   RouterProvider,
+//   Routes,
+//   Route,
+//   useLocation
+// } from "react-router-dom";
+// import { AnimatePresence } from 'framer-motion';
+
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App/>,
+//   },
+//   {
+//     path: "/AboutPersonalSite",
+//     element: <PersonalSite/>,
+//   },
+//   {
+//     path: "/AboutAestige",
+//     element: <Aestige/>,
+//   },
+//   {
+//     path: "/AboutStacked",
+//     element: <Stacked/>,
+//   },
+// ]);
+
+
+
+
+// const RootComponent = () => {
+//   return (
+//     <RouterProvider router={router}>
+//       <MainContent />
+//     </RouterProvider>
+//   );
+// };
+
+// const MainContent = () => {
+//   const location = useLocation(); // <--- This useLocation is nested within the RouterProvider
+
+//   return (
+//     <React.StrictMode>
+//       <AnimatePresence  mode="out-in">
+//         <Routes location={location}>
+//           <Route path="/Home" element={<App />} />
+//           <Route path="/AboutPersonalSite" element={<PersonalSite />} />
+//           <Route path="/AboutAestige" element={<Aestige />} />
+//           <Route path="/AboutStacked" element={<Stacked />} />
+
+//         </Routes>
+//       </AnimatePresence>
+//     </React.StrictMode>
+//   );
+// };
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<RootComponent />);
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import PersonalSite from './pages/PersonalSite';
 import Aestige from './pages/Aestige.jsx';
 import Stacked from './pages/Stacked.jsx';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Routes,
-  Route,
-  useLocation
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AnimatePresence } from 'framer-motion';
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-  },
-  {
-    path: "/AboutPersonalSite",
-    element: <PersonalSite/>,
-  },
-  {
-    path: "/AboutAestige",
-    element: <Aestige/>,
-  },
-  {
-    path: "/AboutStacked",
-    element: <Stacked/>,
-  },
-]);
-
-
-
-
-const RootComponent = () => {
-  return (
-    <RouterProvider router={router}>
-      <MainContent />
-    </RouterProvider>
-  );
-};
+import { useLocation } from 'react-router-dom';
 
 const MainContent = () => {
-  const location = useLocation(); // <--- This useLocation is nested within the RouterProvider
+  const location = useLocation();
 
   return (
     <React.StrictMode>
-      <AnimatePresence  mode="out-in">
+      <AnimatePresence mode='wait'>
         <Routes location={location}>
-          <Route path="/Home" element={<App />} />
+          <Route path="/" element={<App />} />
           <Route path="/AboutPersonalSite" element={<PersonalSite />} />
           <Route path="/AboutAestige" element={<Aestige />} />
           <Route path="/AboutStacked" element={<Stacked />} />
-
         </Routes>
       </AnimatePresence>
     </React.StrictMode>
@@ -63,5 +94,8 @@ const MainContent = () => {
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<RootComponent />);
-
+root.render(
+  <Router>
+    <MainContent />
+  </Router>
+);
