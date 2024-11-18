fb0ce065bf51212d42686abdfc664bbca5044d72
Mike <133874121+Michealebr@users.noreply.github.com>
2024-11-18 19:58:45 +0000

    Update Aestige url link

--- src/pages/Aestige.jsx
+++ src/pages/Aestige.jsx
@@ -16,7 +16,7 @@ const Aestige = () => {
  projectSubTitle={"Freelance Project for a Aesthetics business."}
  
   aboutText={"I connected with a business owner seeking to elevate their brand from just an Instagram page to a professionally tailored website. Through collaborative efforts, we engaged in a series of discussions and design iterations, persistently refining the website until it aligned perfectly with the client's vision."}
-  projecturl="https://aestige.com/" 
+  projecturl="https://aestige.netlify.app" 
   extraText="Ever since this site went live, it has consistently attracted close to 1000 visitors each month, representing a remarkable tenfold increase compared to their Instagram traffic. This surge in visitors has led to a significant uptick in bookings and consultations. My personal goal is to further elevate site traffic to 3-5k visitors monthly. To achieve this, I plan to establish a Google Business account and implement comprehensive website enhancements."
   websiteName="Aestige.com"
  ></AboutProjectLayout>
